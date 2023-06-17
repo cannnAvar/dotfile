719a0e004c5bd3f557f7319a25f0313635d4fa52
cannnAvar <72388136+cannnAvar@users.noreply.github.com>
2023-06-17 14:07:24 +0000

    father i need help

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -3,9 +3,6 @@ local opts =
 {
     noremap = true,      -- non-recursive
     silent = true,       -- do not show message
-    expr = true,
-    replace_keycodes = false,
-    nowait = true
 }
 -----------------
 -- Normal mode --
@@ -45,12 +42,12 @@ function _G.check_back_space()
     local col = vim.fn.col('.') - 1
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
-
 -- Use Tab for trigger completion with characters ahead and navigate
 -- NOTE: There's always a completion item selected by default, you may want to enable
 -- no select by setting `"suggest.noselect": true` in your configuration file
 -- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
 -- other plugins before putting this into your config
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
 vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
 vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 
